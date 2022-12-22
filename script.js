ac9d28b14c6b8ab224d46f0aa505b55b67400b26
Nurjahon1001 <nurjahonalisherova@gmail.com>
2022-12-22 21:28:58 +0100

    init:add js inheritance review

---
+++ script.js
@@ -0,0 +1,95 @@
+class Base {
+  constructor(value) {
+    this.value = value;
+  }
+
+  plus(value) {
+    this.value = this.value + value;
+    return this;
+  }
+
+  minus() {
+    throw new Error('Should be implemented');
+  }
+  divide() {
+    throw new Error('Should be implemented');
+  }
+  mod() {
+    throw new Error('Should be implemented');
+  }
+  multiply() {
+    throw new Error('Should be implemented');
+  }
+  remove() {
+    throw new Error('Should be implemented');
+  }
+  sub() {
+    throw new Error('Should be implemented');
+  }
+
+  get() {
+    console.log(this.value);;
+  }
+}
+
+class IntBuilder extends Base {
+  constructor(value) {
+    super(value);
+  }
+
+  minus(value) {
+    this.value = this.value - value;
+    return this;
+  }
+
+  multiply(value) {
+    this.value = this.value * value;
+    return this;
+  }
+
+  divide(value) {
+    this.value = this.value / value;
+    return this;
+  }
+
+  mod(value) {
+    this.value = Math.floor(this.value / value);
+    return this;
+  }
+}
+
+const test = new IntBuilder(10);
+test.plus(2).minus(4).multiply(3).divide(6).mod(3).get()
+
+function StringBuilder() {
+  Object.call(this, Base(value));
+}
+
+StringBuilder.prototype = Base.prototype.constructor;
+StringBuilder = StringBuilder.prototype;
+
+StringBuilder.prototype.minus = function (value) { 
+  this.value = this.value.slice(0, -value);
+  return this;
+};
+
+StringBuilder.prototype.multiply = function (value) {
+  this.value = this.value.repeat(value);
+  return this;
+};
+
+StringBuilder.prototype.divide = function (value) {
+    this.value = this.value.slice(0,value);
+    return this;
+  };
+  StringBuilder.prototype.remove = function (value) {
+    this.value = this.value.split('l').join('');
+    return this;
+  };
+  StringBuilder.prototype.sub = function (value) {
+    this.value = this.value.substr(value)
+    return this;
+  };
+
+const test2 = new StringBuilder("Hello");
+test2.plus(" all!").minus(4).multiply(3).divide(4).remove('l').sub(1,1).get()
